93e50ca965780042bd1242e1c5d24d25cb4f9954
Nawrez <nawrez.shiliesprit.tn>
2023-11-06 15:59:22 +0100

    FormBuilder

--- src/app/contrat-add/contrat-add.component.spec.ts
+++ src/app/contrat-add/contrat-add.component.spec.ts
@@ -3,6 +3,8 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing'; 
 import { ContratAddComponent } from './contrat-add.component';
 import { KhaddemService } from '../khaddem.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
 describe('ContratAddComponent', () => {
   let component: ContratAddComponent;
   let fixture: ComponentFixture<ContratAddComponent>;
@@ -10,8 +12,8 @@ describe('ContratAddComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ContratAddComponent],
-      imports: [HttpClientTestingModule,RouterTestingModule], 
-      providers: [KhaddemService], 
+      imports: [HttpClientTestingModule, HttpClientModule, RouterTestingModule], 
+      providers: [KhaddemService,FormBuilder], 
     })
     .compileComponents();
 
